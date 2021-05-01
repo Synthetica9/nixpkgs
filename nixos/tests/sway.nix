515630ab5964efe368c5caa1f9c5b76788e964c2
Patrick Hilhorst <git@hilhorst.be>
2021-05-02 00:11:08 +0200

    nixos/tests/sway: refactor

--- nixos/tests/sway.nix
+++ nixos/tests/sway.nix
@@ -3,11 +3,13 @@ import ./make-test-python.nix ({ pkgs, lib, ...} :
 {
   name = "sway";
   meta = {
-    maintainers = with lib.maintainers; [ primeos ];
+    maintainers = with lib.maintainers; [ primeos synthetica ];
   };
 
   machine = { config, ... }: {
     imports = [ ./common/user-account.nix ];
+
+    services.getty.autologinUser = "alice";
 
     programs.sway.enable = true;
 
@@ -22,24 +24,16 @@ import ./make-test-python.nix ({ pkgs, lib, ...} :
     user = nodes.machine.config.users.users.alice;
     XDG_RUNTIME_DIR = "/run/user/${toString user.uid}";
   in ''
-    def login_as_alice():
-        machine.wait_until_tty_matches(1, "login: ")
-        machine.send_chars("alice\n")
-        machine.wait_until_tty_matches(1, "Password: ")
-        machine.send_chars("foobar\n")
-        machine.wait_until_tty_matches(1, "alice\@machine")
-
-
     start_all()
     machine.wait_for_unit("multi-user.target")
+    machine.wait_until_tty_matches(1, "alice\@machine")
     machine.succeed(
         "su - alice -c 'mkdir -p ~/.config/sway && sed s/Mod4/Mod1/ /etc/sway/config > ~/.config/sway/config'"
     )
-    login_as_alice()
     machine.send_chars("sway\n")
     machine.wait_for_file("${XDG_RUNTIME_DIR}/wayland-1")
-    machine.screenshot("empty_workspace")
     machine.sleep(10)
+    machine.screenshot("empty_workspace")
     machine.send_key("alt-ret")
     machine.wait_for_text("alice@machine")
     machine.screenshot("alacritty")
