828045782616f8ea2eea20c558bde9dd79618f51
Michael Weiss <dev.primeos@gmail.com>
2021-05-02 17:19:54 +0200

    nixos/tests/sway: Test XWayland and Wayland

--- nixos/tests/sway.nix
+++ nixos/tests/sway.nix
@@ -8,8 +8,12 @@ import ./make-test-python.nix ({ pkgs, lib, ...} :
 
   machine = { config, ... }: {
     imports = [ ./common/user-account.nix ];
-
     services.getty.autologinUser = "alice";
+
+    # Use a fixed SWAYSOCK path (for swaymsg):
+    environment.variables."SWAYSOCK" = "/tmp/sway-ipc.sock";
+    # For glinfo and wayland-info:
+    environment.systemPackages = with pkgs; [ mesa-demos wayland-utils ];
 
     programs.sway.enable = true;
 
@@ -20,22 +24,48 @@ import ./make-test-python.nix ({ pkgs, lib, ...} :
 
   enableOCR = true;
 
-  testScript = { nodes, ... }: let
-    user = nodes.machine.config.users.users.alice;
-    XDG_RUNTIME_DIR = "/run/user/${toString user.uid}";
-  in ''
+  testScript = { nodes, ... }: ''
     start_all()
     machine.wait_for_unit("multi-user.target")
     machine.wait_until_tty_matches(1, "alice\@machine")
+
+    # Configure and launch Sway:
     machine.succeed(
         "su - alice -c 'mkdir -p ~/.config/sway && sed s/Mod4/Mod1/ /etc/sway/config > ~/.config/sway/config'"
     )
-    machine.send_chars("sway\n")
-    machine.wait_for_file("${XDG_RUNTIME_DIR}/wayland-1")
-    machine.sleep(10)
-    machine.screenshot("empty_workspace")
-    machine.send_key("alt-ret")
+    machine.send_chars("sway && touch exit-ok\n")
+    machine.wait_for_file("/run/user/1000/wayland-1")
+    machine.wait_for_file("/tmp/sway-ipc.sock")
+
+    # Test XWayland:
+    machine.succeed(
+        "su - alice -c 'swaymsg exec WINIT_UNIX_BACKEND=x11 WAYLAND_DISPLAY=invalid alacritty'"
+    )
     machine.wait_for_text("alice@machine")
-    machine.screenshot("alacritty")
+    machine.send_chars("glinfo | head -n 3\n")
+    machine.sleep(5)
+    machine.screenshot("alacritty_glinfo")
+    machine.succeed("pkill alacritty")
+
+    # Start a terminal (Alacritty) on workspace 3:
+    machine.send_key("alt-3")
+    machine.succeed(
+        "su - alice -c 'swaymsg exec WINIT_UNIX_BACKEND=wayland DISPLAY=invalid alacritty'"
+    )
+    machine.wait_for_text("alice@machine")
+    machine.send_chars("wayland-info\n")
+    machine.sleep(5)
+    machine.screenshot("alacritty_wayland_info")
+    machine.send_key("alt-shift-q")
+    machine.wait_until_fails("pgrep alacritty")
+
+    # Test swaynag:
+    machine.send_key("alt-shift-e")
+    machine.wait_for_text("You pressed the exit shortcut.")
+    machine.screenshot("sway_exit")
+
+    # Exit Sway and verify process exit status 0:
+    machine.succeed("su - alice -c 'swaymsg exit || true'")
+    machine.wait_for_file("/home/alice/exit-ok")
   '';
 })
